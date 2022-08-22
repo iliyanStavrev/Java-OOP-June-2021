9ad9ae357a3eaef2734d5fa09a6c5c32b6d10b3d
Iliyan Stavrev <103926682+iliyanStavrev@users.noreply.github.com>
2022-08-22 18:40:42 +0300

    Delete hero directory

--- hero/Main.java
+++
@@ -1,8 +0,0 @@
-package hero;
-
-public class Main {
-    public static void main(String[] args) {
-         MuseElf elf = new MuseElf("Elf",13);
-        System.out.println(elf.toString());
-    }
-}
