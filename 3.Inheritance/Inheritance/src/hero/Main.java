6ccbf6f1bf18b98974bf324572214677ddc7a054
Iliyan Stavrev <103926682+iliyanStavrev@users.noreply.github.com>
2022-08-22 19:01:54 +0300

    Add files via upload

---
+++ 3.Inheritance/Inheritance/src/hero/Main.java
@@ -0,0 +1,8 @@
+package hero;
+
+public class Main {
+    public static void main(String[] args) {
+         MuseElf elf = new MuseElf("Elf",13);
+        System.out.println(elf.toString());
+    }
+}
